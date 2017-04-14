3073455c1654f168222dc8d95937869664ac651d
Shan <info@telekomatrix.com>
2017-04-14 18:19:51 +0100

    Simple Refactoring

--- src/test/java/com/microservice/testing/sample/CricketTeamTest.java
+++ src/test/java/com/microservice/testing/sample/CricketTeamTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+
 public class CricketTeamTest {
+	
+	private static final  int TEAM_POINTS = 22;
 
 	@Test
 	public void constructorShouldSetTeamPts() {
-		CricketTeam team = new CricketTeam(22);
-		assertEquals(22, team.getTeamPts());
+		CricketTeam team = new CricketTeam(TEAM_POINTS);
+		assertEquals("This team won 11 Matches to get this points.", TEAM_POINTS, team.getTeamPts());
 	}
 }
