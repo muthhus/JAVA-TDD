07eeebb65d917d9501e10a216392a3878736d6a6
Shan <info@telekomatrix.com>
2017-04-14 17:36:12 +0100

    Initiated Java Testing Microservice

---
+++ src/main/java/com/microservice/pattern/sample/JavaTestingMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.pattern.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestingMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestingMicroservicesApplication.class, args);
+	}
+}
