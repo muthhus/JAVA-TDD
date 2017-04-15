a5206472a9943310a12027b775dd389538960d6c
Shan <info@telekomatrix.com>
2017-04-15 22:56:04 +0100

    Match class test- RED

---
+++ src/test/java/com/microservice/testing/sample/CricketMatchTest.java
@@ -0,0 +1,12 @@
+package com.microservice.testing.sample;
+
+import org.junit.Test;
+
+public class CricketMatchTest {
+	
+	@Test
+	public void constructorShouldSetMatchDetails(){
+		CricketMatch match = new CricketMatch();
+	}
+
+}
