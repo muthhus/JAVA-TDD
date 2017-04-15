d4a0486671e5a2a5bab2dbe8fc5134203c50b4ae
Shan <info@telekomatrix.com>
2017-04-15 23:02:44 +0100

    Match - Ref and Green

---
+++ src/main/java/com/microservice/testing/sample/CricketMatch.java
@@ -0,0 +1,27 @@
+package com.microservice.testing.sample;
+
+public class CricketMatch {
+	
+	private String mVenue;
+
+	
+	public CricketMatch() {
+
+	}
+
+	public CricketMatch(String mVenue) {
+		this.mVenue = mVenue;
+	}
+
+	public String getmVenue() {
+		return mVenue;
+	}
+
+	public void setmVenue(String mVenue) {
+		this.mVenue = mVenue;
+	}
+	
+	
+	
+
+}
--- src/test/java/com/microservice/testing/sample/CricketMatchTest.java
+++ src/test/java/com/microservice/testing/sample/CricketMatchTest.java
@@ -1,12 +1,19 @@
 package com.microservice.testing.sample;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CricketMatchTest {
 	
+	private static final String MATCH_VENUE ="Chennai";
+	
 	@Test
 	public void constructorShouldSetMatchDetails(){
 		CricketMatch match = new CricketMatch();
+		match.setmVenue("Chennai");
+		
+		assertEquals(MATCH_VENUE, match.getmVenue());
 	}
 
 }
